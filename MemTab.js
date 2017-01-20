6d73ef7f34a33911253c3183f4375bb353bd33cf
Stephen Jin <Stephen Jin>
2017-01-19 20:17:33 -0500

    added the ability to upload a list of terms through a text file

--- MemTab.js
+++ MemTab.js
@@ -4,13 +4,37 @@ var Data = (function() {
 	var term2 = {word: "sac", def: "bag, sack"};
 	var term3 = {word: "voyage", def: "trip, journey"};
 
-	var frenchList = [term, term2, term3];
+	var termList = [term, term2, term3];
+    getTerms(termCallBack);
+     
 
-	var current = frenchList[0];
+	var current;
     var index = 0;
     var progress = 0;
     var timer = 20;
     var finished = false;
+
+    function termCallBack(termVal, indexVal, progressVal, timerVal){
+ 
+        termList = termVal;
+        index = indexVal;
+        current = termList[index];
+        progress = progressVal;
+        timer = timerVal;
+
+        //update element visuals
+        document.getElementById("termDef").innerHTML = current.def;
+        count = timer;
+        document.getElementById("timer").innerHTML=count;
+        updateProgress();
+    }
+
+    function getTerms(callback){
+        chrome.storage.sync.get(null, function(data){
+            callback(data.userTerms, data.currIndex, data.progress, data.timer);
+        });
+
+    }
 
     return {
         getCurrent: function() {
@@ -23,12 +47,12 @@ var Data = (function() {
             return finished;
         },
         nextTerm: function() {
-           if(index < frenchList.length-1){
+           if(index < termList.length-1){
             index = index+1;
-            current = frenchList[index];
+            current = termList[index];
             progress = ++progress;
            }
-           else if(index == frenchList.length-1){ //last term in the list
+           else if(index == termList.length-1){ //last term in the list
             progress = ++progress;
             $('#finished-alert').show();
             $("#inputText").attr("readonly", "");
@@ -36,38 +60,56 @@ var Data = (function() {
            }
         },
         getProgress: function(){
-            if (frenchList.length > 0){
-            var percent = (progress/frenchList.length)*100;
+            if (termList.length > 0){
+            var percent = (progress/termList.length)*100;
             return percent;
             }
             
         },
         restart: function(){
             index = 0;
-            current = frenchList[index];
+            current = termList[index];
             progress = 0;
+        },
+        setTimer: function(time){
+            timer = time;
+        },
+        addTerm: function(term){
+            termList.push(term);
+        },
+        clearTerms: function(){
+            termList.length = 0;
+        },
+        setTerms: function(){
+            chrome.storage.sync.set({'userTerms': termList}, function(){
+            });
+        },
+        updateData: function(){
+            chrome.storage.sync.set({'currIndex': index, 'progress': progress, 'timer': timer}, function(){
+            });
         }
-       
+
+        
     };
 
 })();
 
 //main driver function
 (function(){
-	var curr = Data.getCurrent();
-document.getElementById("termDef").innerHTML = curr.def;
+	//var curr = Data.getCurrent();
+//document.getElementById("termDef").innerHTML = curr.def;
 
 $("#success-alert").hide();
 $("#wrong-alert").hide();
-})();
 
+})();
 
 
 //toggles play and pause button
 var counter;
 var count = Data.getTimer();
 $('#play-toggle').click( function(){
-    $(this).find('i').toggleClass("glyphicon-play").toggleClass("glyphicon-pause");
+    $(this).find('i').toggleClass("glyphicon-pause").toggleClass("glyphicon-play");
 
 
     if($(this).find('i').hasClass("glyphicon-pause")){
@@ -115,14 +157,54 @@ $('#restart').click(function(e){
     var curr = Data.getCurrent();
     document.getElementById("termDef").innerHTML = curr.def;
     updateProgress();
+    Data.updateData();
 
-    //start timer if play button has been pressed
-    if($("#play-toggle").find('i').hasClass("glyphicon-pause")){
-    resetTimer();
-    }
-
+    //reset play/pause button and timer
+    $("#play-toggle").find('i').removeClass("glyphicon-play").addClass("glyphicon-pause");
+    $('#play-toggle').click();
+    count = Data.getTimer();
+    document.getElementById("timer").innerHTML=count;
+    document.getElementById("inputText").value = "";
+    
 });
 
+//dropdown menu
+ $(document).ready(function () {
+        $('.dropdown-toggle').dropdown();
+    });
+
+$(".dropdown-menu a").click(function() {
+    $(this).closest(".dropdown-menu").prev().dropdown("toggle");
+});
+
+//trigger file upload from the options dropdown menu
+ $("#upload").on('click', function(e){
+        e.preventDefault();
+        alert("Select a text file with each term on a new line in the following format:\n\nterm: definition\n\ne.g.\nboat: a small vessel propelled on water by oars, sails, or an engine");
+        $("#file:hidden").trigger('click');
+    });
+
+//prompt the user to enter a timer value
+$("#setTimer").on('click', function(){
+    var value = prompt("Enter a value in seconds");
+
+    //if the value is a positive integer, update the timer
+    if(isInt(value) && value > 0){
+        Data.setTimer(value);
+        Data.updateData();
+        count = Data.getTimer();
+        document.getElementById("timer").innerHTML=count;
+    }
+
+
+  //check if value is an integer
+  function isInt(value) {
+  if (isNaN(value)) {
+    return false;}
+  var x = parseFloat(value);
+  return (x | 0) === x;
+    }
+});
 
 
 
@@ -156,6 +238,7 @@ function refreshTerm(){
     setTimeout(function() {
     document.getElementById("termDef").innerHTML = curr.def;
     updateProgress();
+    Data.updateData();
     resetForm();
     document.getElementById("inputText").value = "";
 
@@ -211,6 +294,37 @@ function updateProgress(){
     $('.progress-bar').css('width', value+'%').attr('aria-valuenow', value);  
 
 }
+
+//reads in a text file of terms and loads the terms onto the page
+document.getElementById('file').onchange = function(){
+
+  var file = this.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function(progressEvent){
+
+    //Read line by line
+    var lines = this.result.split('\n');
+    Data.clearTerms();
+    for(var line = 0; line < lines.length; line++){
+       var str = lines[line].split(/: |:/); 
+      console.log(str[0]);
+      console.log(str[1]);
+      var term = {word: str[0], def: str[1]};
+      Data.addTerm(term);
+    }
+
+    Data.setTerms();
+    
+    //reset list to beginning on file load
+    $('#restart').click();
+    Data.updateData();
+    window.location.reload();
+  };
+
+  reader.readAsText(file);
+};
+
 
 
 
